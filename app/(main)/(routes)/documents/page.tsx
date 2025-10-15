5217334700980f329d8b380fe2ace4e03009c377
KC Kim <kckim@KCs-MacBook-Pro.local>
2025-10-14 22:53:12 -0700

    adding avatar on sidebar and logout button

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return (
-        <div>
-            <h1>Documents</h1>
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image
+                src="/team.svg"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="dark:hidden"
+            />
+            <Image
+                src="/team-dark.svg"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="hidden dark:block"
+            />
+            <h2 className="text-2xl font-medium">
+                Welcome to {user?.firstName}&apos;s Notion X
+            </h2>
+            <Button>
+                <PlusCircle className="w-4 h-4"/>
+                Create a note
+            </Button>
         </div>
     );
 }
